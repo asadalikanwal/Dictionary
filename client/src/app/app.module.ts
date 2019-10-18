a0d413b8a7f97b0b5a3914af7a023b2430e8d3b6
asadalikanwal <asad@indivirtualdubai.com>
2019-10-18 14:53:19 -0500

    removing un-necessary files

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,16 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { UserService } from './user.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
