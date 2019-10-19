b81c598d5b61d442c48e400af51ff31bf701d66a
asadalikanwal <asad@indivirtualdubai.com>
2019-10-18 19:11:44 -0500

    Adding JWT, Bcrypt and dotenv

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 
-const atlasUrl = `mongodb+srv://asadalikanwal:${process.env.DB_PASSWORD}@cluster-oaqxp.gcp.mongodb.net/dictionary?retryWrites=true&w=majority`
+const atlasUrl = `mongodb+srv://asadalikanwal:asadalikanwal@cluster-oaqxp.gcp.mongodb.net/dictionary?retryWrites=true&w=majority`
 mongoose.connect(atlasUrl, (err) => {
     if(!err){
         console.log("Database is connected successfully");
+        console.log("process.env.DB_PASSWORD", process.env.DB_PASSWORD);
     } else {
         console.log("Database failed to connect", err);
     }
