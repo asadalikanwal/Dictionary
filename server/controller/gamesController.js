8ba25c626ae07f6f5e975cc3e8776a1d862a1989
asadalikanwal <asad@indivirtualdubai.com>
2019-10-20 11:03:35 -0500

    Creating a get request

--- server/controller/gamesController.js
+++ server/controller/gamesController.js
@@ -1,15 +1,34 @@
 const router = require('express').Router();
 const Game = require('../models/games');
 
+// To start a new game
 router.post('/', (req, res, next) => {
-    
+    console.log("wroks");
     const newGame = new Game(req.body);
-    console.log("LEts check", newGame);
     newGame.save((err, docs) => {
         if(!err) {
             res.send(docs);
         } else {
             return next 
+        }
+    })
+})
+
+// To find a game by gameID
+router.get('/:gameid', (req, res, next) => {
+    Game.findById(req.params.gameid, (err, doc) => {
+        if(!err) {
+            if(doc){
+                res.send(doc);
+            } else {
+                next({
+                    "message": "Not able to find the game"
+                })
+            }
+        } else {
+            return next({
+                "message": "Not able to find the game"
+            })
         }
     })
 })
